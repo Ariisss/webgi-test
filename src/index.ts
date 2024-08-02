3ca4feb2be0153e7dfe604264a252b9c3948da97
Ariisss <arispantino@gmail.com>
2024-08-02 19:25:49 +0800

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import {
     GBufferPlugin,
     timeout,
     ProgressivePlugin,
-    TonemapPlugin,
+    // TonemapPlugin,
     SSRPlugin,
     SSAOPlugin,
     DiamondPlugin,
@@ -20,7 +20,8 @@ import {
     ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
 
     IViewerPlugin, FileTransferPlugin,
-    ARPlugin
+    ARPlugin,
+    PerspectiveCamera, 
 
     // Color, // Import THREE.js internals
     // Texture, // Import THREE.js internals
@@ -33,22 +34,6 @@ async function setupViewer(){
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
     })
-
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
-    // and many more...
 
     // or use this to add all main ones at once.
     await addBasePlugins(viewer) // check the source: https://codepen.io/repalash/pen/JjLxGmy for the list of plugins added.
@@ -63,18 +48,34 @@ async function setupViewer(){
     // await viewer.addPlugin(CanvasSnipperPlugin)
 
     // Add AR plugin
-    await viewer.addPlugin(ARPlugin)
+    // const AR = new ARPlugin();
+    // await viewer.addPlugin(ARPlugin)
+
+    // Add a camera to the scene
+    const cameraObj = new PerspectiveCamera();
+    // viewer.scene.activeCamera = viewer.createCamera(cameraObj);
+    viewer.scene.activeCamera = viewer.createCamera(cameraObj);
+    const activeCamera = viewer.scene.activeCamera;
+
+    console.log(viewer.scene.activeCamera)
 
     // Import and add a GLB file.
-    await viewer.load("./assets/Bracelet AR Model 08.glb")
+    // await viewer.load("./assets/Bracelet AR Model 08.glb")
     
     // Load an environment map if not set in the glb file
     // await viewer.setEnvironmentMap("./assets/environment.hdr");
 
     // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
+    // const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
     // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+    // uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+
+    const ARButton = document.getElementById('arButton') as HTMLButtonElement
+    ARButton.onclick = async () => {
+        // AR.enterAR();
+        console.log("AR Button Clicked!")
+        // console.log(xrSession)
+    }
 
 }
 
